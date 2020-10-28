f57c7b4606a17edce326c9345e7bea5a362a64e2
Hugo Guimarães Maia <hugoguima75@outlook.com>
2020-10-28 00:43:21 -0300

    Retirado trechos de código JavaScript redundantes.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 window.onscroll = function() {myFunction()};
 
+//Pega o Botão
 var navbar = document.getElementById("navigation");
 
+//Pega a posição inicial
 var sticky = navbar.offsetTop;
 
-
+//Adiciona a classe de fixação a barra de pesquisa
 function myFunction() {
-    
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
@@ -15,22 +16,11 @@ function myFunction() {
 }
 
 
-//Get the button:
+//Pega o Botão
  var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
+ 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; //  Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE e Opera
 }
